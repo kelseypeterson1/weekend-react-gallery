59b78a2b5107706ee0850530e49546ce35598260
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-02-18 21:22:06 -0600

    started to create conditional rendering

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,8 +1,38 @@
-function GalleryItem({image}) {
-    return (
-        <div>
-            <img src={image.url} alt={image.name}></img>
-        </div>
+import { useEffect, useState } from 'react';
+
+function GalleryItem({ image }) {
+
+    const { imageSelected, setImageSelected } = useState(false);
+    const { spaceRendering, setSpaceRendering } = useState("");
+
+    console.log(image.name, "imageSelected state is", imageSelected)
+
+    const handleClick = () => {
+        setImageSelected(!imageSelected);
+    }
+
+    const spaceCondRender = () => {
+        if (imageSelected === false) {
+            console.log(image.name, "show picture")
+            // setSpaceRendering(
+                // )
+            } else {
+                // setSpaceRendering(
+                    console.log(image.name, "show description")
+                    // <p>{image.name}</p>
+                    // )
+                }
+            }
+            
+            spaceCondRender()
+            
+            return (
+                <>
+            <div onClick={handleClick}>
+            {imageSelected ? <img src={image.url} alt={image.name}></img> : <p>description here</p>}
+                </div>
+                {/* // {spaceRendering}; */}
+        </>
     )
 }
 
